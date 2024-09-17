a2b92e79676435bd3b83dcfc20e8001de6402da3
NancyG07 <nancy08garciag@gmail.com>
2024-09-16 19:54:39 -0600

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,162 @@
+class Game {
+  constructor() {
+    this.resetTitle = createElement("h2");
+    this.resetButton = createButton("");
+    this.leaderboarTitle = createElement("h2");
+    this.leader1 = createElement("h2");
+    this.leader2 = createElement("h2");
+  }
+
+  start(){
+    form = new Form();
+    form.display();
+    car1 = createSprite(width/2 - 50, height - 100);
+    car1.addImage("car1", car1_img);
+    car1.scale = 0.07;
+
+    car2 = createSprite(width/2 + 100, height - 100);
+    car2.addImage("car2", car2_img);
+    car2.scale = 0.07;
+
+    cars = [car1, car2];
+    fuels = new Group();
+    powerCoins = new Group();
+
+    //agregar sprites de combustible
+     this.addSprites(fuels, 4, fuelImage, 0.02);
+
+     //agregar sprites de combustible
+     this.addSprites(powerCoins, 18, powerCoinsImage, 0.09);
+
+    player = new Player();
+    playerCount = player.getCount();
+  }
+
+  addSprites(spriteGroup,numberOfSprites,spriteImage,scale){
+    for(var i = 0; i<numberOfSprites; i++){
+      var x,y;
+      x = random(width / 2 + 150, width / 2 -150);
+      y = random(-height * 4.5, height-400);
+
+      var sprite = createSprite(x,y);
+      sprite.addImage("sprite", spriteImage);
+      sprite.scale = scale;
+      spriteGroup.add(sprite);
+    }
+  }
+
+  getState(){
+    var gameStateRef = database.ref("gameState");
+    gameStateRef.on("value", function(data){
+      gameState = data.val();
+    })
+  }
+
+  update(state){
+    database.ref("/").update({
+      gameState: state
+    })
+  }
+
+  handleElements(){
+    form.hide();
+    form.titleImg.position(40, 50);
+    form.titleImg.class("gameTitleAfterEffect");
+    this.resetTitle.html("Reiniciar juego");
+    this.resetTitle.class("resetText");
+    this.resetTitle.position(width/2 + 200, 40);
+
+    this.resetButton.class("resetButton");
+    this.resetButton.position(width / 2 + 230, 100);
+
+    this.leaderboarTitle.html("puntuacion");
+    this.leaderboarTitle.class("resetText");
+    this.leaderboarTitle.position(width/3 - 60, 40);
+
+    this.leader1.class("leadersText");
+    this.leader1.position(width/3 -50, 80);
+
+    this.leader2.class("leadersText");
+    this.leader2.position(width/3 -50, 130);
+  }
+
+  play(){
+    this.handleElements();
+    this.handleResetButtos();
+    Player.getPlayersInfo();
+    if(allPlayers !== undefined){
+      image(track, 0, -height * 5, width, height *6);
+      this.showLeaderboar();
+      //indice de la matriz
+      var index = 0;
+      for(var plr in allPlayers){
+        //agregar 1 al indice para cada bucle
+        index = index + 1;
+        //utilizar datos de la base de datos para mostrar los autos en las direccciones de x e y
+        var x = allPlayers[plr].positionX;
+        var y = height - allPlayers[plr].positionY;
+        cars[index - 1].position.x = x;
+        cars[index - 1].position.y = y;
+        if(index === player.index){
+          stroke(10);
+          fill("red");
+          ellipse(x, y, 60, 60);
+          //cambiar la posicion de la camara en la direccion de y
+          camera.position.x = cars[index -1].position.x;
+          camera.position.y = cars[index -1].position.y;                 
+        }
+      }
+      this.handlePlayerControls();
+      drawSprites();
+    }
+  }
+
+  showLeaderboar(){
+    var leader1, leader2;
+    var players = Object.values(allPlayers);
+    if((players[0].rank === 0 && players[1].rank === 0) || players[0].rank === 1){
+      //&emsp es una etiqueta que se utiliza para mostar cuatro espacios
+      leader1 =
+      players[0].rank +"&emsp;" + players[0].name +"&emsp;" + players[0].score;
+      
+     leader2=
+      players[1].rank +"&emsp;" + players[1].name +"&emsp;" + players[1].score;
+  }
+  if(players[1].rank === 1){
+    leader1 =
+    players[1].rank +"&emsp;" + players[1].name +"&emsp;" + players[1].score;
+    
+    leader2=
+    players[0].rank +"&emsp;" + players[0].name +"&emsp;" + players[0].score;
+  }
+  this.leader1.html(leader1);
+  this.leader2.html(leader2);
+}
+
+  handleResetButtos(){
+    this.resetButton.mousePressed(() =>{
+      database.ref("/").set({
+        playerCount: 0,
+        gameState: 0,
+        players:{}
+      });
+    window.location.reload();
+    })
+  }
+  handlePlayerControls(){
+    //manejar eventos del teclado
+    if(keyIsDown(UP_ARROW)){
+      player.positionY += 10;
+      player.update();
+    }
+    if(keyIsDown(LEFT_ARROW) && player.positionX > width / 3 - 50){
+      player.positionX -= 5;
+      player.update();
+    }
+    if(keyIsDown(RIGHT_ARROW) && player.positionX < width/2 + 300){
+      player.positionX +=5;
+     player.update();
+    }
+  }
+  
+}
